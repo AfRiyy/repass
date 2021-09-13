c59d48f0fdc073c946abfe8bdbb9b67e2244d832
AfRiyy <AfRiyy@gmail.com>
2021-09-13 08:28:31 +0200

    cleaning

--- src/App.java
+++ src/App.java
@@ -4,53 +4,64 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class App {
+    private static Scanner scan;
+    private static  String user;
+    private static  String pass;
+    private static  String place;
     public static void main(String[] args) {
-        nevjegy();
-        //Fejrész kiírása
-        System.out.println("Jelszavak");
-        //Verzió kiírása
-        System.out.println("Verzió: 0.0.1");
-        
-        //Az a objektummal kérhetünk be a konzolról
-        Scanner scan = new Scanner(System.in); //az a nem megfelelő változónév
-
-        System.out.print("Felhasználónév: ");
-        // A b változó tárolja a jelszót
-        String username = scan.nextLine();//a b nem megfelelő változónév
-        System.out.print("Jelszó: ");
-        String pass = scan.nextLine();//a c nem megfelelő változónév
-        System.out.print("Hely: ");
-        String place = scan.nextLine(); //a d nem megfelelő változónév
-        scan.close(); 
+        aboutShow();
+        scan = new Scanner(System.in); 
+        setUser();
+        setPass();
+        setPlace();
+        scan.close();
         int isSucceed = 0;
+        
+        if (isSucceed == 1) {
+            System.out.println("Ok. A kiírás sikeres.");
+        } else {
+            System.out.println("Hiba! A kiírás sikertelen");
+        }
+
+    }
+
+    public static void aboutShow() {
+        System.out.println("Nagy János");
+        // Fejrész kiírása
+        System.out.println("Jelszavak");
+        // Verzió kiírása
+        System.out.println("Verzió: 0.0.1");
+    }
+    public static void setUser(){
+        System.out.print("Felhasználónév: ");
+        user = scan.nextLine();
+    }
+    public static void setPass(){
+        System.out.print("Jelszó: ");
+        pass = scan.nextLine();
+    }
+    public static void setPlace(){
+        System.out.print("Hely: ");
+        place = scan.nextLine();
+    }
+    public static void profileWriteOut(){
         try {
-            /* 
-            A jelszó, a felhasználónév és a 
-            használati helye a passList 
-            objektumban van tárolva            
-            */
-            Store passList = new Store(username, pass, place);
+            /*
+             * A jelszó, a felhasználónév és a használati helye a passList objektumban van
+             * tárolva
+             */
+            Profile profile = new Profile(user, pass, place);
             FileWriter fileWriter = new FileWriter("pass.txt");
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.print(passList.user);
-            if(!passList.hollow()) { printWriter.print(passList.retrieval()); }
-            printWriter.print(passList.place);
+            printWriter.print(profile.user);
+            if (!profile.isPassEmpty()) {
+                printWriter.print(profile.getPass());
+            }
+            printWriter.print(profile.place);
             printWriter.close();
             isSucceed = 1;
         } catch (IOException ex) {
             System.err.println("Hiba! A fájlbaírás sikertelen. Keresse meg a fejlesztőt.");
         }
-
-        if(isSucceed == 1) { 
-            System.out.println("Ok. A kiírás sikeres.");  
-        }
-        else {  
-            System.out.println("Hiba! A kiírás sikertelen"); 
-        }
-
-    }
-
-    public static void nevjegy() {
-        System.out.println("Nagy János");
     }
 }
